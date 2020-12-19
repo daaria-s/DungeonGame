54d84c070081032a0307cb9e35016daf32684323
WELC0ME <zz-crazy-zz@mail.ru>
2020-12-19 17:25:48 +0300

    add object classes

--- objects.py
+++ objects.py
@@ -1,18 +1,45 @@
-class Wall:
-    pass
+import pygame
+import config
 
 
-class Empty:
-    pass
+class Object:
+
+    def __init__(self, image, position, name):
+        self.image = pygame.transform.scale(pygame.image.load(image),
+                                            (config.TILE, config.TILE))
+        self.position = position
+        self.name = name
+
+    def show(self):
+        pass
 
 
-class Teleport:
-    pass
+class Wall(Object):
+
+    def __init__(self, position):
+        super().__init__('Sprites/wall.png', position, 'wall')
 
 
-class Box:
-    pass
+class Empty(Object):
+
+    def __init__(self, position):
+        super().__init__('Sprites/ground.png', position, 'empty')
 
 
-class Chest:
-    pass
+class Teleport(Object):
+
+    def __init__(self, position, number):
+        super().__init__('Sprites/ground.png', position, 'teleport')
+        self.number = number
+
+
+class Box(Object):
+
+    def __init__(self, position):
+        super().__init__('Sprites/ground.png', position, 'box')  # поставить правильный спрайт
+
+
+class Chest(Object):
+
+    def __init__(self, position):
+        super().__init__('Sprites/ground.png', position, 'chest')  # поставить правильный спрайт
