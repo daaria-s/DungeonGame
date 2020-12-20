bd3981ff6bd56643309dbd1962e905a20ed87d0e
WELC0ME <zz-crazy-zz@mail.ru>
2020-12-20 12:51:57 +0300

    add function to create animation

--- animations.py
+++ animations.py
@@ -1,3 +1,6 @@
+from config import *
+
+
 class Iterator:
 
     def __init__(self, values, n=None):
@@ -29,3 +32,36 @@ class Animation:
 
     def reset(self):
         self.iterator.reset()
+
+
+class UnknownAnimation(Exception):
+    pass
+
+
+def new(animation_name):
+    if animation_name == 'IDLE':
+        return Animation('IDLE', Iterator([(0, -1), (0, -1), (0, -2), (0, -2), (0, -3), (0, -3),
+                                           (0, -2), (0, -2), (0, -1), (0, -1), (0, 0), (0, 0),
+                                           (0, 1), (0, 1), (0, 2), (0, 2), (0, 3), (0, 3),
+                                           (0, 2), (0, 2), (0, 1), (0, 1), (0, 0), (0, 0)]))
+    elif animation_name == 'RIGHT_MOVE':
+        return Animation('RIGHT_MOVE', Iterator([(i, 0) for i in range(TILE, 0, -4)], TILE // 4),)
+    elif animation_name == 'LEFT_MOVE':
+        return Animation('LEFT_MOVE', Iterator([(-i, 0) for i in range(TILE, 0, -4)], TILE // 4))
+    elif animation_name == 'UP_MOVE':
+        return Animation('UP_MOVE', Iterator([(0, -i) for i in range(TILE, 0, -4)], TILE // 4))
+    elif animation_name == 'DOWN_MOVE':
+        return Animation('DOWN_MOVE', Iterator([(0, i) for i in range(TILE, 0, -4)], TILE // 4))
+
+    elif animation_name == 'RIGHT_ATTACK':
+        return Animation('RIGHT_ATTACK', (Iterator([(abs(TILE - i) - TILE, 0) for i in range(0, 2 * TILE, 4)], TILE // 2)))
+    elif animation_name == 'LEFT_ATTACK':
+        return Animation('LEFT_ATTACK', Iterator([(TILE - abs(TILE - i), 0) for i in range(0, 2 * TILE, 4)], TILE // 2))
+    elif animation_name == 'UP_ATTACK':
+        return Animation('UP_ATTACK', Iterator([(0, TILE - abs(TILE - i)) for i in range(0, 2 * TILE, 4)], TILE // 2))
+    elif animation_name == 'DOWN_ATTACK':
+        return Animation('DOWN_ATTACK', Iterator([(0, abs(TILE - i) - TILE) for i in range(0, 2 * TILE, 4)], TILE // 2))
+
+    elif animation_name == 'DIE':
+        return Animation('DIE', Iterator([(0, 0)]))
+    raise UnknownAnimation(animation_name)
