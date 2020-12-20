185f6da8b395e48411ba80b8134d09d9b7c8470c
WELC0ME <zz-crazy-zz@mail.ru>
2020-12-20 12:01:26 +0300

    add animation class

--- animations.py
+++ animations.py
@@ -1,2 +1,31 @@
+class Iterator:
+
+    def __init__(self, values, n=None):
+        if len(values) == 0:
+            raise IndexError
+        self.values = values
+        self.counter = 0
+        self.limit = n
+
+    def __next__(self):
+        if not self.limit or self.counter < self.limit:
+            self.counter += 1
+            return self.values[(self.counter - 1) % len(self.values)]
+        else:
+            raise StopIteration
+
+    def reset(self):
+        self.counter = 0
+
+
 class Animation:
-	pass
+
+    def __init__(self, name, iterator):
+        self.name = name
+        self.iterator = iterator
+
+    def __next__(self):
+        return next(self.iterator)
+
+    def reset(self):
+        self.iterator.reset()
