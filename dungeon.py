d6f90c8605793a0f8dcd2243100974ab76ead574
WELC0ME <zz-crazy-zz@mail.ru>
2021-01-04 19:26:48 +0300

    change dungeon way of storing information

--- dungeon.py
+++ dungeon.py
@@ -28,15 +28,18 @@ class Dungeon:
         self.coordinates = {
             (1, 1): Player((1, 1)),
             (5, 2): Enemy((5, 2)),
-            (2, 3): Enemy((2, 3))
+            (2, 3): Enemy((2, 3)),
+            (3, 0): Teleport((3, 0), 2)
         }
+        self.player = [obj for obj in self.coordinates.values() if obj.name == 'player'][0]
+        self.enemies = [obj for obj in self.coordinates.values() if obj.name == 'enemy']
 
     def get(self, coords, diff=(0, 0)):
         return self.coordinates.get([coords[1] + diff[1]][coords[0] + diff[0]])
 
     def player_move(self, button):
 
-        if any([self.get(i).entity.animation.name not in ['IDLE', 'DIE'] for i in self.entities_position[1:] if self.get(i).entity]):
+        if any([i.animation.name not in ['IDLE', 'DIE'] for i in self.enemies]):
             return
 
         buttons_keys = {
@@ -46,62 +49,32 @@ class Dungeon:
             pygame.K_DOWN: (0, 1)
         }
 
-        player = self.get(self.entities_position[0]).entity
-
-        if player.animation.name != 'IDLE':
+        if self.player.animation.name != 'IDLE':
             return
         if button not in buttons_keys.keys():
             return
 
-        target = self.get(self.entities_position[0], buttons_keys[button])
-
-        if not target:
-            return
-
-        player.interaction(target)
-        self.get(self.entities_position[0]).entity = None
-        self.entities_position[0] = player.position
-        self.entities_direction[0] = (-buttons_keys[button][0], -buttons_keys[button][1])
-        self.get(player.position).entity = player
+        self.player.interaction(self.get(self.player.position, buttons_keys[button]))
 
     def enemies_move(self):
 
-        if self.get(self.entities_position[0]).entity.animation.name != 'IDLE':
+        if self.player.animation.name != 'IDLE':
             return
 
         options = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         res = []
 
-        for i in range(1, len(self.entities_position)):
-            if self.get(self.entities_position[i]).entity and not self.get(self.entities_position[i]).entity.alive:
-                self.get(self.entities_position[i]).entity = None
-                continue
-            if not self.get(self.entities_position[i]).entity:
-                continue
-            if self.get(self.entities_position[i]).entity.name != 'enemy':
-                continue
-
+        for enemy in self.enemies:
             diff = options[random.randint(0, len(options) - 1)]
-            enemy = self.get(self.entities_position[i]).entity
 
             if enemy.animation.name != 'IDLE':
                 res.append(True)
                 continue
 
-            target = self.get(self.entities_position[i], diff)
-
-            if not target:
-                return
-
+            target = self.get(enemy.position, diff)
             res.append(enemy.interaction(target))
-            self.get(self.entities_position[i]).entity = None
-            self.entities_position[i] = enemy.position
-            self.entities_direction[i] = (-diff[0], -diff[1])
-            self.get(self.entities_position[i]).entity = enemy
 
         if not any(res):
             config.TURN = 1
-            for i in range(1, len(self.entities_position)):
-                if self.get(self.entities_position[i]).entity:
-                    enemy = self.get(self.entities_position[i]).entity
-                    enemy.action_points = enemy.max_action_points
+            for enemy in self.enemies:
+                enemy.action_points = enemy.max_action_points
